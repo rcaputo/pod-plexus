e963e12227d70c36d4123c9d2457cc6b8f535708
Rocco Caputo <rcaputo@cpan.org>
2011-06-06 01:08:54 -0400

    Initial Dist::Zilla plugin, and some library changes to support it.

---
+++ dist-zilla-plugin-podplexis/lib/Dist/Zilla/Plugin/PodPlexus.pm
@@ -0,0 +1,51 @@
+package Dist::Zilla::Plugin::Plexus;
+
+use Moose;
+with qw(
+	Dist::Zilla::Role::FileMunger
+);
+
+use Moose::Autobox;
+use Pod::Plexus::Library;
+
+has library => (
+	default => sub { Pod::Plexus::Library->new() },
+	is      => 'rw',
+	isa     => 'Pod::Plexus::Library',
+	lazy    => 1,
+);
+
+sub munge_files {
+	my $self = shift();
+
+	# Index all files.  Done as a single pass before actual munging
+	# since we don't know which files need which index information.
+
+	foreach my $file ($self->zilla()->files()->flatten()) {
+
+		# We only want things that include POD.
+		# TODO - Is there a better way?
+		next unless $file->name() =~ /^(?:bin|lib)\//;
+
+		$self->library()->add_file($file->name());
+	}
+
+	# Munge each file individually.
+
+	my $documents = $self->library()->documents();
+	foreach my $file ($self->zilla()->files()->flatten()) {
+		my $path = $file->name();
+
+		next unless exists $documents->{$path};
+
+		my $doc = $documents->{$path};
+
+		$doc->collect_ancestry();
+		$doc->expand_commands();
+
+		$file->content( $doc->render() );
+	}
+}
+
+no Moose;
+1;
--- pod-plexus/lib/Pod/Plexus/Library.pm
+++ pod-plexus/lib/Pod/Plexus/Library.pm
@@ -5,6 +5,8 @@ use File::Find;
 use Template;
 use Carp qw(confess);
 
+use Pod::Plexus::Document;
+
 has documents => (
 	is      => 'rw',
 	isa     => 'HashRef[Pod::Plexus::Document]',
@@ -51,19 +53,25 @@ sub add_files {
 			wanted => sub {
 				return if $self->has_document($_) or not $filter->($_);
 
-				my $document = Pod::Plexus::Document->new(
-					pathname => $_,
-					library  => $self,
-					template => $self->template(),
-				);
-
-				$self->add_document($_ => $document);
-				$self->add_module($document->module() => $document);
+				$self->add_file($_);
 			},
 			no_chdir => 1,
 		},
 		@roots,
 	);
+}
+
+sub add_file {
+	my ($self, $file_path) = @_;
+
+	my $document = Pod::Plexus::Document->new(
+		pathname => $file_path,
+		library  => $self,
+		template => $self->template(),
+	);
+
+	$self->add_document($file_path => $document);
+	$self->add_module($document->module() => $document);
 }
 
 no Moose;
